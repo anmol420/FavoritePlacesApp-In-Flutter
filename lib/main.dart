ddb4fca83b71339f079f8d08e70460fa8b449398
Anmol Anand <76034132+anmol420@users.noreply.github.com>
2023-12-29 08:39:13 +0530

    Added Providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:favorite_places_app/screens/places.dart';
 
@@ -28,7 +29,9 @@ final theme = ThemeData().copyWith(
 
 void main() {
   runApp(
-      const MyApp(),
+    const ProviderScope(
+      child: MyApp(),
+    ),
   );
 }
 
